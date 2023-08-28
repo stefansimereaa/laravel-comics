df9e00a67c71265cae70b358b182f298cdfced64
stefansimereaa <stefanosimerea16@gmail.com>
2023-08-28 13:41:42 +0200

    fixed code

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Comics</title>
+    <title>DC Comics</title>
     <link href="{{ Vite::asset('resources/img/favicon.ico') }}" rel="icon" type="image/ico">
 
     @vite('resources/js/app.js')
