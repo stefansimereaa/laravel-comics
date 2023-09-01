cd756aba63f8c8cb833a97275ecfd1f7c87b4ef5
stefansimereaa <stefanosimerea16@gmail.com>
2023-09-01 09:24:31 +0200

    fixed code for controller

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -7,9 +7,7 @@ use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         $comics = Comic::all();
@@ -17,18 +15,14 @@ class ComicController extends Controller
         return view('comics.index', compact('comics', 'shop_items'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
         $comic = new Comic();
         return view('comics.create', compact('comic'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         $request->validate([
@@ -48,25 +42,19 @@ class ComicController extends Controller
         return to_route('comics.show', $new_comic);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Comic $comic)
     {
         return view('comics.show', compact('comic'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(Comic $comic)
     {
         return view('comics.edit', compact('comic'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, Comic $comic)
     {
         $request->validate([
@@ -84,9 +72,7 @@ class ComicController extends Controller
         return to_route('comics.show', $comic);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Comic $comic)
     {
         $comic->delete();
